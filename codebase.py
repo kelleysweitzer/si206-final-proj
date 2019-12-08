4aea34cbf72235cc3a5ea376d7d633cab6103d68
Kelley <kjsweitz@umich.edu>
2019-12-08 18:43:13 -0500

    fixed the issue with the SQL Insert

--- codebase.py
+++ codebase.py
@@ -32,32 +32,27 @@ if token:
     # start sql table
     cur.execute("DROP TABLE IF EXISTS Songlist")
     cur.execute("CREATE TABLE Songlist (id TEXT PRIMARY KEY, title TEXT, artist TEXT, duration INTEGER, popularity INTEGER)")
-    while i < 20:  
+    total = sp.current_user_saved_tracks(limit=1)['total']
 
-        # makes API call for 20 items
-        results = sp.current_user_saved_tracks(limit=20, offset=last_index)
-        
+    while last_index < total or last_index < 100:  
+
+        try:
+            # makes API call for 20 items
+            results = sp.current_user_saved_tracks(limit=20, offset=last_index)
+        except:
+            pass
+
+
         for item in results['items']:
-            _id = item['track']['id']
-            _title = item['track']['name']
-            _artist = item['track']['artists'][0]['name']
-            _duration = item['track']['duration_ms']
-            _popularity = item['track']['popularity']
-            last_index += 1
-            cur.execute("INSERT INTO Songlist (id,title,artist,duration,popularity) VALUES (?,?,?,?,?)",(_id, _title,_artist,_duration,_popularity))
-            conn.commit()
+                _id = item['track']['id']
+                _title = str(item['track']['name'])
+                _artist = item['track']['artists'][0]['name']
+                _duration = item['track']['duration_ms']
+                _popularity = item['track']['popularity']
+                last_index += 1
+                cur.execute("INSERT INTO Songlist (id,title,artist,duration,popularity) VALUES (?,?,?,?,?)",(_id, _title,_artist,_duration,_popularity))
+                conn.commit()
 
-        i+= 1
-        
-
-
-
-    
-    
-    # # print(results)
-    # for item in results['items']:
-    #     track = item['track']
-    #     print (track['genre'] + ' - ' + track['artists'][0]['name'])
 else:
     print ("Can't get token for", username)
 
--- musixmatch_interactive.py
+++ musixmatch_interactive.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sqlite3
 import os 
+import time
 from musixmatch_lyrics_api import * 
 
 
@@ -14,24 +15,28 @@ last_index = 0
 # start sql table
 cur.execute("DROP TABLE IF EXISTS lyricslist")
 cur.execute("CREATE TABLE lyricslist (id INTEGER PRIMARY KEY, title TEXT, artist TEXT, lyrics TEXT)")
-pairs = cur.execute("SELECT title, artist FROM Songlist LIMIT 10")
-
+pairs = cur.execute("SELECT title, artist FROM songlist")
+pairs = pairs.fetchmany(300)
 counter = 1
 
-for row in pairs: 
-    track_name = row[0]
-    print(track_name)
-    artist_name = row[1]
-    print(artist_name)
-    print(row)
-    #Finding the lyrics of a song: base url + api method + different parameters + the input that the user put in + API key 
-    api_call = base_url + lyrics_matcher + format_url + artist_search_parameter + artist_name + track_search_parameter + track_name + api_key 
-    #call the API 
-    request = requests.get(api_call)
-    data = request.json()
-    #data is equivalent to whatever is down at this level of message and body 
-    data = data["message"]["body"]
-    lyrics = data["lyrics"]["lyrics_body"]
-    cur.execute("INSERT INTO lyricslist (id, title, artist, lyrics) VALUES (?, ?, ?, ?)", (counter, track_name, artist_name, lyrics))
-    conn.commit()
-    counter = counter + 1
+for pair in pairs: 
+    try:
+        track_name = pair[0]
+        print(track_name)
+        artist_name = pair[1]
+        print(artist_name)
+
+        # Finding the lyrics of a song: base url + api method + different parameters + the input that the user put in + API key 
+        api_call = base_url + lyrics_matcher + format_url + artist_search_parameter + artist_name + track_search_parameter + track_name + api_key 
+        #call the API 
+        request = requests.get(api_call)
+        data = request.json()
+        #data is equivalent to whatever is down at this level of message and body 
+        data = data["message"]["body"]
+        lyrics = data["lyrics"]["lyrics_body"]
+        if lyrics == "":
+            cur.execute("INSERT INTO lyricslist (id, title, artist, lyrics) VALUES (?, ?, ?, ?)", (counter, track_name, artist_name, lyrics))
+            conn.commit()
+        counter = counter + 1
+    except:
+        pass
