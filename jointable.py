2e7f8a21987c31c64b3c792556c95ec399c51e07
Sanya Verma <verma.sanya@gmail.com>
2019-12-16 01:32:36 -0500

    join table- sadtimes

--- jointable.py
+++ jointable.py
@@ -1,12 +1,18 @@
 import json
 import os 
 import sqlite3
+import requests
+
 #JOIN table with sad songs 
 # start sql table
+
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+"songlist.db")
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS sadtimes')
-cur.execute('CREATE TABLE sadtimes(id INTEGER PRIMARY KEY, title TEXT, artist TEXT, duration INTEGER, sad_score INTEGER, sad TEXT)')
-cur.execute("SELECT * FROM sadsongs INNER JOIN Songlist ON sadsongs.title=Songlist.title")
+cur.execute("CREATE TABLE sadtimes (id	INTEGER, title TEXT, artist TEXT, sad TEXT, sad_score INTEGER, duration INTEGER)"
+
+cur.execute("INSERT INTO sadtimes SELECT sadsongs.*, Songlist.duration, Songlist.title FROM sadsongs INNER JOIN Songlist ON sadsongs.title=Songlist.title")
+cur.execute()
+
