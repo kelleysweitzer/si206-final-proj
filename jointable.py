fb90a93dee991a704473125fd7f552495850d147
Sanya Verma <verma.sanya@gmail.com>
2019-12-16 16:07:21 -0500

    python jpin worksgit add . yay

--- jointable.py
+++ jointable.py
@@ -2,7 +2,6 @@ import json
 import os 
 import sqlite3
 import requests
-
 #JOIN table with sad songs 
 # start sql table
 
@@ -11,19 +10,19 @@ conn = sqlite3.connect(path+'/'+"songlist.db")
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS sadtimes')
-cur.execute("CREATE TABLE sadtimes (id	INTEGER, title TEXT, artist TEXT, sad TEXT, sad_score INTEGER, duration INTEGER)"
+cur.execute("CREATE TABLE sadtimes (id INTEGER, title TEXT, artist TEXT, sad TEXT, sad_score INTEGER, duration INTEGER)")
 
-cur.execute("INSERT INTO sadtimes SELECT sadsongs.*, Songlist.duration, Songlist.title FROM sadsongs INNER JOIN Songlist ON sadsongs.title=Songlist.title")
+cur.execute("INSERT INTO sadtimes SELECT sadsongs.*, Songlist.duration FROM sadsongs INNER JOIN Songlist ON sadsongs.title=Songlist.title")
 conn.commit()
 
 #avg duration of sad songs in sadtimes 
-cur.execute("SELECT avg(duration) FROM sadtimes") 
-conn.commit()
+#cur.execute("SELECT avg(duration) FROM sadtimes") 
+#conn.commit()
 
 #avg sad_score in sadsongs 
-cur.execute("SELECT avg(sad_score)FROM sadtimes")
-conn.commit()
+#cur.execute("SELECT avg(sad_score)FROM sadtimes")
+#conn.commit()
 
 #avg duration in songlist 
-cur.execute("SELECT avg(duration) FROM songlist")
-conn.commit()
+#cur.execute("SELECT avg(duration) FROM songlist")
+#conn.commit()
