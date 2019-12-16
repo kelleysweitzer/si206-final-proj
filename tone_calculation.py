95ab7dcd56a01ff15b61649a8fa3a0aec8c9c836
Sanya Verma <verma.sanya@gmail.com>
2019-12-16 18:43:55 -0500

    avg primary_score

--- tone_calculation.py
+++ tone_calculation.py
@@ -4,6 +4,7 @@ import sqlite3
 import json
 import unittest
 
+#connecting to database 
 conn = sqlite3.connect("songlist.db")
 cur = conn.cursor()
 
@@ -15,52 +16,81 @@ analytical = 0
 sadness = 0
 joy = 0 
 anger = 0 
-confident = 0 
+confident = 0
+confident_time = 0
 fear = 0 
 
+#getting total number of tentative songs and average primary_score
 tentative_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Tentative"')
+tentative_sum = 0
 for row in tentative_songs: 
-    if row[2] == "Tentative":
-        tentative += 1
-tone_dict["Tentative"] = tentative
+    tentative += 1
+    tentative_sum = row[3]
+tentative_average = tentative_sum/tentative
+lst = [tentative, tentative_average]
+tone_dict["Tentative"] = lst 
 
+#getting total number of analytical songs and average primary_score
 analytical_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Analytical"')
+analytical_sum = 0 
 for row in analytical_songs: 
-    if row[2] == "Analytical":
-        analytical += 1
-tone_dict["Analytical"] = analytical
+    analytical += 1
+    analytical_sum = row[3]
+analytical_average = analytical_sum/analytical
+lst = [analytical, analytical_average]
+tone_dict["Analytical"] = lst 
 
+#getting total number of sadness songs and average primary_score
 sadness_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Sadness"')
+sadness_sum =0 
 for row in sadness_songs: 
-    if row[2] == "Sadness":
-        sadness += 1
-tone_dict["Sadness"] = sadness
+    sadness += 1
+    sadness_sum = row[3]
+sadness_average = sadness_sum/sadness
+lst = [sadness, sadness_average]
+tone_dict["Sadness"] = lst 
 
+#getting total number of joy songs and average primary_score
 joy_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Joy"')
+joy_sum = 0
 for row in joy_songs: 
-    if row[2] == "Joy":
-        joy += 1
-tone_dict["Joy"] = joy
+    joy += 1
+    joy_sum = row[3]
+joy_average = joy_sum/joy
+lst = [joy, joy_average]
+tone_dict["Joy"] = lst 
 
+#getting total number of angry songs and average primary_score
 anger_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Anger"')
+anger_sum = 0 
 for row in anger_songs: 
-    if row[2] == "Anger":
-        anger += 1
-tone_dict["Anger"] = anger
+    anger += 1
+    anger_sum = row[3]
+anger_average = anger_sum/anger
+lst = [anger, anger_average]
+tone_dict["Anger"] = lst 
 
+#getting total number of confident songs and average primary_score
 confident_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Confident"')
+confident_sum = 0
 for row in confident_songs: 
-    if row[2] == "Confident":
-        confident += 1
-tone_dict["Confident"] = confident
+    confident += 1
+    confident_sum += row[3]
+confident_average = confident_sum/confident
+lst = [confident, confident_average]
+tone_dict["Confident"] = lst
 
+#getting total number of fear songs and average primary_score
 fear_songs = cur.execute('SELECT title, artist, primary_tone, primary_score FROM tonelist WHERE primary_tone = "Fear"')
+fear_sum = 0 
 for row in fear_songs: 
-    if row[2] == "Fear":
-        fear += 1
-tone_dict["Fear"] = fear
+    fear += 1
+    fear_sum += row[3]
+fear_average = fear_sum/fear 
+lst = [fear, fear_average]
+tone_dict["Fear"] = lst 
 
-
+#turning dictionary into a json file 
 try:
     filename = open('tonelist.json', 'r')
     file_results = json.loads(filename)
